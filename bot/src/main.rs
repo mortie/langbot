3a8224ff0f1dfdc38f1b58b1bea15ad4dae81329
Martin Dørum <martid0311@gmail.com>
2022-04-30 20:52:08 +0200

    discord bot essentially works now

---
+++ bot/src/main.rs
@@ -0,0 +1,124 @@
+mod podmanager;
+
+use std::env;
+use std::sync::Mutex;
+
+use lazy_static::lazy_static;
+use podmanager::PodManager;
+use regex::{Regex, RegexBuilder};
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::model::id::UserId;
+use serenity::prelude::*;
+
+lazy_static! {
+    static ref INLINE_CODE_RX: Regex = {
+        let pattern = r"^<@\d+>\s+(\w+).*`(.*?)`";
+        Regex::new(pattern).unwrap()
+    };
+    static ref MULTILINE_CODE_RX: Regex = {
+        let pattern = r"^<@\d+>\s+(\w+).*```(.*?)```";
+        RegexBuilder::new(pattern)
+            .dot_matches_new_line(true)
+            .build()
+            .unwrap()
+    };
+}
+
+struct Handler {
+    id: Mutex<Option<UserId>>,
+    podman: Mutex<podmanager::PodManager>,
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        // Ignore messages from bots (avoid endless loop)
+        if msg.author.bot {
+            return;
+        }
+
+        // Ignore messages which don't mention us
+        let my_id = self.id.lock().unwrap().unwrap();
+        if msg.mentions.iter().find(|&m| m.id == my_id).is_none() {
+            return;
+        }
+
+        let caps = INLINE_CODE_RX
+            .captures(&msg.content)
+            .or_else(|| MULTILINE_CODE_RX.captures(&msg.content));
+        let caps = match caps {
+            Some(caps) => caps,
+            None => return,
+        };
+
+        let language =  caps.get(1).unwrap().as_str().to_lowercase();
+        let content = caps.get(2).unwrap().as_str();
+
+        let pod = self.podman.lock().unwrap().get_pod();
+        let pod = match pod {
+            Ok(pod) => pod,
+            Err(err) => {
+                eprintln!("Couldn't get pod: {}", err);
+                let res = msg.channel_id.say(&ctx.http, format!("Error: {}", err)).await;
+                if let Err(err) = res {
+                    eprintln!("Error sending message: {:?}", err);
+                }
+
+                return;
+            }
+        };
+
+        let output = match pod.execute(&language, &content) {
+            Ok(output) => output,
+            Err(err) => {
+                eprintln!("Couldn't execute code: {}", err);
+                let res = msg.channel_id.say(&ctx.http, format!("Error: {}", err)).await;
+                if let Err(err) = res {
+                    eprintln!("Error sending message: {:?}", err);
+                }
+
+                return;
+            }
+        };
+
+        let res = msg.channel_id.say(&ctx.http, format!("Result: {}", output)).await;
+        if let Err(err) = res {
+            eprintln!("Error sending message: {:?}", err);
+        }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a
+    // shard is booted, and a READY payload is sent by Discord. This payload
+    // contains data like the current user's guild Ids, current user data,
+    // private channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    async fn ready(&self, _: Context, ready: Ready) {
+        *self.id.lock().unwrap() = Some(ready.user.id);
+        eprintln!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a DISCORD_TOKEN in the environment");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let handler = Handler {
+        id: Mutex::new(None),
+        podman: Mutex::new(PodManager::new("langbot".into())),
+    };
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        eprintln!("Client error: {:?}", why);
+    }
+}
---
+++ bot/src/podmanager.rs
@@ -0,0 +1,171 @@
+use std::process::{Command, Stdio};
+use std::io::Write;
+
+pub struct Pod {
+    id: String,
+}
+
+impl Pod {
+    fn new_from_tag(tag: &str) -> Result<Pod, String> {
+        // id="$(podman container create "$tag")"
+        // podman container start "$id"
+
+        let output = Command::new("podman")
+            .arg("container")
+            .arg("create")
+            .arg(tag)
+            .arg("sleep")
+            .arg("10")
+            .stderr(Stdio::inherit())
+            .output();
+        let output = match output {
+            Ok(output) => output,
+            Err(err) => return Err(format!("Creating pod failed: {}", err)),
+        };
+
+        if !output.status.success() {
+            return Err("Creating pod failed".into());
+        }
+
+        let id = match String::from_utf8(output.stdout) {
+            Ok(id) => id.trim().to_string(),
+            Err(err) => return Err(format!("Creating pod failed: {}", err)),
+        };
+
+        let output = Command::new("podman")
+            .arg("container")
+            .arg("start")
+            .arg(&id)
+            .stderr(Stdio::inherit())
+            .output();
+        let output = match output {
+            Ok(output) => output,
+            Err(err) => return Err(format!("Creating pod failed: {}", err)),
+        };
+
+        if !output.status.success() {
+            return Err("Starting pod failed".into());
+        }
+
+        Ok(Pod { id })
+    }
+
+    pub fn execute(&self, language: &str, content: &str) -> Result<String, String> {
+        // echo "$content" | podman exec -i "$id" ./scripts/run.sh "$language"
+
+        let child = Command::new("podman")
+            .arg("exec")
+            .arg("-i")
+            .arg(&self.id)
+            .arg("./scripts/run.sh")
+            .arg(language)
+            .stdin(Stdio::piped()).stdout(Stdio::piped()).stderr(Stdio::piped())
+            .spawn();
+        let mut child = match child {
+            Ok(child) => child,
+            Err(err) => return Err(format!("Running program failed: {}", err)),
+        };
+
+        if let Some(stdin) = &mut child.stdin {
+            match stdin.write_all(&content.as_bytes()) {
+                Ok(()) => (),
+                Err(err) => return Err(format!("Running program failed: {}", err)),
+            }
+        }
+
+        let output = match child.wait_with_output() {
+            Ok(output) => output,
+            Err(err) => return Err(format!("Running program failed: {}", err)),
+        };
+
+        let mut errmsg: Option<String> = None;
+        {
+            let msg = String::from_utf8_lossy(&output.stderr).trim().to_string();
+            if msg.len() > 0 {
+                errmsg = Some(msg);
+            }
+        }
+
+        let mut outmsg: Option<String> = None;
+        {
+            let msg = String::from_utf8_lossy(&output.stdout).trim().to_string();
+            if msg.len() > 0 {
+                outmsg = Some(msg);
+            }
+        }
+
+        if !output.status.success() {
+            let exiterr = match output.status.code() {
+                Some(code) => format!("Code {}", code),
+                None => "Exited by signal".into(),
+            };
+
+            if let Some(errmsg) = errmsg {
+                return Err(format!("Running program failed: {}\nSTDERR: {}", exiterr, errmsg));
+            } else {
+                return Err(format!("Running program failed: {}", exiterr));
+            }
+        }
+
+        if errmsg.is_some() && outmsg.is_some() {
+            Ok(format!("STDERR: {}\n\n{}", errmsg.unwrap(), outmsg.unwrap()))
+        } else if let Some(errmsg) = errmsg {
+            Ok(format!("STDERR: {}", errmsg))
+        } else if let Some(outmsg) = outmsg {
+            Ok(format!("{}", outmsg))
+        } else {
+            Ok("(No output)".into())
+        }
+    }
+}
+
+impl Drop for Pod {
+    fn drop(&mut self) {
+        // podman container rm "$id"
+
+        let output = Command::new("podman")
+            .arg("container")
+            .arg("kill")
+            .arg(&self.id)
+            .output();
+        match output {
+            Ok(output) => output,
+            Err(err) => {
+                eprintln!("Killing container {} failed: {}", self.id, err);
+            }
+        };
+        // We don't really care if the kill command fails; that just means
+        // the container has already exited
+
+        let output = Command::new("podman")
+            .arg("container")
+            .arg("rm")
+            .arg(&self.id)
+            .stderr(Stdio::inherit())
+            .output();
+        let output = match output {
+            Ok(output) => output,
+            Err(err) => {
+                eprintln!("Removing container {} failed: {}", self.id, err);
+                return;
+            }
+        };
+        if !output.status.success() {
+            eprintln!("Removing container {} failed", self.id);
+        }
+    }
+}
+
+pub struct PodManager {
+    tag: String,
+}
+
+impl PodManager {
+    pub fn new(tag: String) -> Self {
+        Self { tag }
+    }
+
+    pub fn get_pod(&self) -> Result<Pod, String> {
+        Pod::new_from_tag(&self.tag)
+    }
+}
