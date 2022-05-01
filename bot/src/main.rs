7a37ef4e63d50f89a6f358f5cc84ac38ba00cd53
Martin Dørum <martid0311@gmail.com>
2022-05-01 03:59:13 +0200

    respond to message edits

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,15 +1,18 @@
 mod podmanager;
 
+use std::collections::HashMap;
 use std::env;
 use std::sync::Mutex;
 
 use lazy_static::lazy_static;
-use podmanager::PodManager;
+use podmanager::{ExecResult, PodManager};
 use regex::{Regex, RegexBuilder};
 use serenity::async_trait;
+use serenity::builder::CreateEmbed;
 use serenity::model::channel::Message;
+use serenity::model::event::MessageUpdateEvent;
 use serenity::model::gateway::Ready;
-use serenity::model::id::UserId;
+use serenity::model::id::{ChannelId, MessageId, UserId};
 use serenity::prelude::*;
 use serenity::utils::Color;
 
@@ -27,12 +30,7 @@ lazy_static! {
     };
 }
 
-struct Handler {
-    id: Mutex<Option<UserId>>,
-    podman: Mutex<podmanager::PodManager>,
-}
-
-fn truncate_string(text: String) -> String {
+fn truncate_string(text: &String) -> String {
     const CHLIMIT: usize = 800;
     const LINELIMIT: usize = 20;
 
@@ -88,131 +86,203 @@ fn zws_encode(text: String) -> String {
     text.replace("`", "`\u{200B}")
 }
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        // Ignore messages from bots (avoid endless loop)
-        if msg.author.bot {
-            return;
-        }
+struct Handler {
+    id: Mutex<Option<UserId>>,
+    podman: Mutex<podmanager::PodManager>,
+    responses: Mutex<HashMap<(ChannelId, MessageId), (ChannelId, MessageId)>>,
+}
 
-        // Ignore messages which don't mention us
-        let my_id = self.id.lock().unwrap().unwrap();
-        if msg.mentions.iter().find(|&m| m.id == my_id).is_none() {
-            return;
-        }
+fn create_embed_from_result(output: ExecResult, embed: &mut CreateEmbed) {
+    if !output.status.success() {
+        let code = match output.status.code() {
+            Some(code) => match exit_code_to_desc(code) {
+                Some(desc) => format!("{} ({})", code, desc),
+                None => format!("{}", code),
+            },
+            None => "Signal".to_string(),
+        };
 
+        embed.description(format!("Exit Code {}", code));
+        embed.color(Color::DARK_RED);
+    } else {
+        embed.description(format!("Exit Code 0 (OK)"));
+        embed.color(Color::DARK_GREEN);
+    }
+
+    if let Some(stdout) = &output.stdout {
+        embed.field(
+            "STDOUT",
+            format!("```ansi\n{}\n```", zws_encode(truncate_string(stdout))),
+            false,
+        );
+    }
+
+    if let Some(stderr) = &output.stderr {
+        embed.field(
+            "STDERR",
+            format!("```ansi\n{}\n```", zws_encode(truncate_string(stderr))),
+            false,
+        );
+    }
+}
+
+impl Handler {
+    fn parse_and_run_text(&self, text: &str) -> Option<Result<ExecResult, String>> {
         let caps = MULTILINE_CODE_RX
-            .captures(&msg.content)
-            .or_else(|| INLINE_CODE_RX.captures(&msg.content));
+            .captures(text)
+            .or_else(|| INLINE_CODE_RX.captures(text));
         let caps = match caps {
             Some(caps) => caps,
-            None => return,
+            None => return None,
         };
 
         let language = caps.get(1).unwrap().as_str().to_lowercase();
         let content = caps.get(2).unwrap().as_str();
 
-        let _typing = match msg.channel_id.start_typing(&ctx.http) {
-            Ok(t) => Some(t),
-            Err(err) => {
-                eprintln!("Failed to send start_typing message: {}", err);
-                None
-            }
-        };
-
         let pod = self.podman.lock().unwrap().get_pod();
         let pod = match pod {
             Ok(pod) => pod,
-            Err(err) => {
-                eprintln!("Couldn't get pod: {}", err);
-                if let Err(err) = msg
-                    .channel_id
-                    .say(&ctx.http, format!("Error: {}", err))
-                    .await
-                {
-                    eprintln!("Error sending response: {}", err);
-                }
-                return;
-            }
+            Err(err) => return Some(Err(err)),
         };
 
         let output = match pod.execute(&language, &content) {
             Ok(output) => output,
+            Err(err) => return Some(Err(err)),
+        };
+
+        Some(Ok(output))
+    }
+
+    fn parse_and_run(&self, msg: &Message) -> Option<Result<ExecResult, String>> {
+        // Ignore messages from bots
+        if msg.author.bot {
+            return None;
+        }
+
+        // Ignore messages which don't mention us
+        let my_id = self.id.lock().unwrap().unwrap();
+        if msg.mentions.iter().find(|&m| m.id == my_id).is_none() {
+            return None;
+        }
+
+        self.parse_and_run_text(&msg.content)
+    }
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message_update(&self, ctx: Context, evt: MessageUpdateEvent) {
+        let response = match self
+            .responses
+            .lock()
+            .unwrap()
+            .get(&(evt.channel_id, evt.id))
+        {
+            Some(response) => *response,
+            None => return,
+        };
+        let (response_channel, response_id) = response;
+
+        let content = match evt.content {
+            Some(content) => content,
+            None => return,
+        };
+
+        let output = match self.parse_and_run_text(&content) {
+            Some(output) => output,
+            None => return,
+        };
+
+        let output = match output {
+            Ok(output) => output,
             Err(err) => {
-                eprintln!("Couldn't execute code: {}", err);
-                if let Err(err) = msg
-                    .channel_id
-                    .say(&ctx.http, format!("Error: {}", err))
-                    .await
-                {
-                    eprintln!("Error sending response: {}", err);
+                eprintln!("Error: {}", err);
+                let resp = response_channel
+                    .edit_message(&ctx.http, response_id, |edit| {
+                        edit.content(format!("Error: {}", err))
+                    })
+                    .await;
+                if let Err(err) = resp {
+                    eprintln!("Couldn't edit message: {}", err);
                 }
                 return;
             }
         };
 
-        let res = msg
-            .channel_id
-            .send_message(&ctx.http, |m| {
-                m.embed(|e| {
-                    if !output.status.success() {
-                        let code = match output.status.code() {
-                            Some(code) => match exit_code_to_desc(code) {
-                                Some(desc) => format!("{} ({})", code, desc),
-                                None => format!("{}", code),
-                            },
-                            None => "Signal".to_string(),
-                        };
-
-                        e.description(format!("Exit Code {}", code));
-                        e.color(Color::DARK_RED);
-                    } else {
-                        e.description(format!("Exit Code 0 (OK)"));
-                        e.color(Color::DARK_GREEN);
-                    }
-
-                    if let Some(stdout) = output.stdout {
-                        e.field(
-                            "STDOUT",
-                            format!("```ansi\n{}\n```", zws_encode(truncate_string(stdout))),
-                            false,
-                        );
-                    }
-
-                    if let Some(stderr) = output.stderr {
-                        e.field(
-                            "STDERR",
-                            format!("```ansi\n{}\n```", zws_encode(truncate_string(stderr))),
-                            false,
-                        );
-                    }
-
-                    e
+        let resp = response_channel
+            .edit_message(&ctx.http, response_id, |edit| {
+                edit.embed(|embed| {
+                    create_embed_from_result(output, embed);
+                    embed
                 })
             })
             .await;
-        if let Err(err) = res {
-            eprintln!("Error sending response: {}", err);
-            let _ = msg
-                .channel_id
-                .say(&ctx.http, format!("Couldn't send response: {}", err))
-                .await;
+        match resp {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("Couldn't edit message: {}", err);
+                let resp = response_channel
+                    .edit_message(&ctx.http, response_id, |edit| {
+                        edit.content(format!("Error: {}", err))
+                    })
+                    .await;
+                if let Err(err) = resp {
+                    eprintln!("Couldn't edit message: {}", err);
+                }
+            }
         }
-
-        /*
-        if let Err(err) = msg.reply(&ctx.http, format_output(output)).await {
-            eprintln!("Error sending response: {}", err);
-        }
-        */
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
+    async fn message(&self, ctx: Context, msg: Message) {
+        let output = match self.parse_and_run(&msg) {
+            Some(output) => output,
+            None => return,
+        };
+
+        let output = match output {
+            Ok(output) => output,
+            Err(err) => {
+                eprintln!("Error: {}", err);
+                if let Err(err) = msg
+                    .channel_id
+                    .say(&ctx.http, format!("Error: {}", err))
+                    .await
+                {
+                    eprintln!("Couldn't send error: {}", err);
+                }
+                return;
+            }
+        };
+
+        let resp = msg
+            .channel_id
+            .send_message(&ctx.http, |m| {
+                m.embed(|embed| {
+                    create_embed_from_result(output, embed);
+                    embed
+                })
+            })
+            .await;
+        match resp {
+            Ok(reply) => {
+                self.responses
+                    .lock()
+                    .unwrap()
+                    .insert((msg.channel_id, msg.id), (reply.channel_id, reply.id));
+            }
+            Err(err) => {
+                eprintln!("Couldn't send message: {}", err);
+                if let Err(err) = msg
+                    .channel_id
+                    .say(&ctx.http, format!("Error: {}", err))
+                    .await
+                {
+                    eprintln!("Couldn't send error: {}", err);
+                }
+            }
+        }
+    }
+
     async fn ready(&self, _: Context, ready: Ready) {
         *self.id.lock().unwrap() = Some(ready.user.id);
         eprintln!("{} is connected!", ready.user.name);
@@ -229,6 +299,7 @@ async fn main() {
     let handler = Handler {
         id: Mutex::new(None),
         podman: Mutex::new(PodManager::new("langbot".into())),
+        responses: Mutex::new(HashMap::new()),
     };
 
     let mut client = Client::builder(&token, intents)
