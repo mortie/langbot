73c90e6c4e3acdb89c355129d0a39c80e3208b68
Martin Dørum <martid0311@gmail.com>
2022-05-02 19:31:30 +0200

    racket: automatically prepend '#lang racket' if no '#' exists

--- langs/racket/run.sh
+++ langs/racket/run.sh
@@ -1,2 +1,10 @@
-cat >input.rkt
+cat >input.rkt.in
+if head -n 1 input.rkt.in | grep '^\s*#' >/dev/null; then
+	mv input.rkt.in input.rkt
+else
+	echo '#lang racket' >input.rkt
+	cat input.rkt.in >>input.rkt
+	rm input.rkt.in
+fi
+
 exec racket input.rkt
