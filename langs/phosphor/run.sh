b4459e599614a02d7a046687589b8e112cd915be
Benedikt Magnus <magnus@magnuscraft.de>
2022-05-02 19:01:46 +0200

    End Phosphor run script with non-zero exit code if compilation fails

--- langs/phosphor/run.sh
+++ langs/phosphor/run.sh
@@ -5,6 +5,11 @@ cat > input.ph
 cd phosphorCompiler
 npm start -- -t linuxAmd64 -s ../phosphorStandardLibrary ../input.ph ../output
 
+if [ ! -f ../output ]; then
+    # If no output exists, the compilation did fail and we need to exit with a non-zero exit code:
+    exit 1
+fi
+
 # Execute the programme:
 cd ..
 exec ./output
