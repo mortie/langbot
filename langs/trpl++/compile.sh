4f8714150894809e05bc0924ddbc63a206be648a
Martin Dørum <martid0311@gmail.com>
2022-05-03 22:23:18 +0200

    fix some trpl recipe stuff

--- langs/trpl++/compile.sh
+++ langs/trpl++/compile.sh
@@ -1,4 +1,12 @@
 git clone https://git.coredoes.dev/e3team/turbo-rpl
 cd turbo-rpl
+git checkout dd75f256acce0e8897d1b55369218e987d679693
+
+# Remove call to stty, it won't run in a TTY
+sed -i '/system("stty erase/d' main.cpp
+
 g++ -std=c++17 -o trpl main.cpp -pthread
+
+cp trpl "$DEPLOYDIR"
+
 touch "$DEPLOYDIR/.done"
