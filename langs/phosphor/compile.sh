046842b37ffe109b23535b96f32d63d7f4603902
Martin Dørum <martid0311@gmail.com>
2022-05-05 19:59:35 +0200

    only compile phosphor if on linux x86_64

--- langs/phosphor/compile.sh
+++ langs/phosphor/compile.sh
@@ -1,18 +1,21 @@
-git clone https://github.com/PhosphorLang/PhosphorStandardLibrary.git
-cd PhosphorStandardLibrary
-git checkout de8733a71b13946380646cffe44a68483b91575b
-./build.sh linuxAmd64
-cd ..
+if [ "$(uname -sm)" = "Linux x86_64" ]; then
+	git clone https://github.com/PhosphorLang/PhosphorStandardLibrary.git
+	cd PhosphorStandardLibrary
+	git checkout de8733a71b13946380646cffe44a68483b91575b
+	./build.sh linuxAmd64
+	cd ..
 
-git clone https://github.com/PhosphorLang/PhosphorCompiler.git
-cd PhosphorCompiler
-git checkout 1015e9c3a15487ceb238fc1f8e250a1e79a56cf2
-npm install
-npm run build
-cd ..
+	git clone https://github.com/PhosphorLang/PhosphorCompiler.git
+	cd PhosphorCompiler
+	git checkout 1015e9c3a15487ceb238fc1f8e250a1e79a56cf2
+	npm install
+	npm run build
+	cd ..
 
-cp -r PhosphorStandardLibrary/bin "$DEPLOYDIR/PhosphorStandardLibrary"
-cp -r PhosphorCompiler "$DEPLOYDIR"
+	cp -r PhosphorStandardLibrary/bin "$DEPLOYDIR/PhosphorStandardLibrary"
+	cp -r PhosphorCompiler "$DEPLOYDIR"
+else
+	touch "$DEPLOYDIR/.unsupported-system"
+fi
 
-# Sanity confirmation:
 touch "$DEPLOYDIR/.done"
