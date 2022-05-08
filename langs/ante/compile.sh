4be4166ae906c5e6d8c83cb79ed4f68b8838f773
Martin Dørum <martid0311@gmail.com>
2022-05-09 00:19:30 +0200

    make ante not mess with ~/.config

--- langs/ante/compile.sh
+++ langs/ante/compile.sh
@@ -2,7 +2,7 @@ git clone https://github.com/jfecher/ante.git
 cd ante
 git checkout 1df3e650de2347b73b46b142e3abb6b87b015356
 
-cargo build --release
+XDG_CONFIG_HOME="$DEPLOYDIR/.config" cargo build --release
 cp target/release/ante "$DEPLOYDIR"
 
 touch "$DEPLOYDIR/.done"
