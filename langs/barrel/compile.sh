4896b692c260bb1950a751a70486416a9fb6b2f5
crabbo-rave <vatisprocurator@gmail.com>
2022-05-30 12:33:04 -0400

    added barrel

---
+++ langs/barrel/compile.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/the-cellar/barrel
+cd barrel
+raco pkg install 
+raco setup
+
+touch "$DEPLOYDIR/.done"
---
+++ langs/barrel/run.sh
@@ -0,0 +1,6 @@
+cd barrel 
+git pull 
+cd ..
+
+cat >input.brl 
+brl input.brl
