f4e40eca493b3cda97f9ee73dbceafa4820bc19b
UberPyro <uberpyro181@gmail.com>
2022-06-03 09:27:25 -0400

    Update prowl

--- langs/prowl/compile.sh
+++ langs/prowl/compile.sh
@@ -1,11 +1,11 @@
 git clone https://github.com/UberPyro/prowl.git
 cd prowl
-git checkout 0651712a3bd3285d164ac915745ef3905de105f3
+git checkout 6c108913ad3e66e7dd9741ad500a27a3ba75906b
 
 opam init
 eval `opam env`
 
-opam install -y dune batteries ppx_deriving menhir
+opam install -y dune batteries ppx_deriving menhir alcotest
 
 dune build
 cp _build/default/prowl.exe "$DEPLOYDIR"
