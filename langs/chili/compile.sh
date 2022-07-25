69ecbb6e5a47075b174e891ced08d1eeb1068f7f
r0nsha <r0nsh4v1t@gmail.com>
2022-07-25 19:46:11 +0300

    update commit

--- langs/chili/compile.sh
+++ langs/chili/compile.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/r0nsha/chili-lang.git --recurse-submodules chili
 cd chili
-git checkout 505151a99abe6ba996bdd5a0282ceebd592c8a6c
+git checkout 24d08d24d420046a7ce1ed9285a996cc1c9961e9
 
 cargo build --release
 cp target/release/chili "$DEPLOYDIR"
